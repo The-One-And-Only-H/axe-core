47aada2d072eb06d3cbe78c5183b332b6cc9985d
jkodu <jey.nandakumar@gmail.com>
2020-01-14 18:15:54 +0000

    update tests

--- test/core/utils/preload-media.js
+++ test/core/utils/preload-media.js
@@ -6,7 +6,6 @@ describe.only('axe.utils.preloadMedia', function() {
 	var origFn = axe.utils.preloadMedia;
 	var fixture = document.getElementById('fixture');
 	var fixtureSetup = axe.testUtils.fixtureSetup;
-	var isIE11 = axe.testUtils.isIE11;
 
 	afterEach(function() {
 		axe.utils.preloadMedia = origFn;
@@ -23,39 +22,56 @@ describe.only('axe.utils.preloadMedia', function() {
 
 		axe._tree = axe.utils.getFlattenedTree(document);
 
-		axe.utils.preloadMedia({ treeRoot: axe._tree[0] }).then(function() {
-			assert.ok(isCalled);
-			done();
-		});
+		axe.utils
+			.preloadMedia({ treeRoot: axe._tree[0] })
+			.then(function() {
+				assert.ok(isCalled);
+				done();
+			})
+			.catch(function(err) {
+				done(err);
+			});
 	});
 
 	it('returns empty array when there are no media nodes to be preloaded', function(done) {
 		axe._tree = axe.utils.getFlattenedTree(document);
 
-		axe.utils.preloadMedia({ treeRoot: axe._tree[0] }).then(function(result) {
-			assert.equal(result.length, 0);
-			done();
-		});
-	});
-
-	(isIE11 ? it.skip : it)(
-		'returns empty array when <audio> has no source',
-		function(done) {
-			fixtureSetup('<audio autoplay="true" controls></audio>');
-
-			axe.utils.preloadMedia({ treeRoot: axe._tree[0] }).then(function(result) {
+		axe.utils
+			.preloadMedia({ treeRoot: axe._tree[0] })
+			.then(function(result) {
 				assert.equal(result.length, 0);
 				done();
+			})
+			.catch(function(err) {
+				done(err);
 			});
-		}
-	);
+	});
 
-	it('returns empty array when <video> has no source', function(done) {
+	it('returns empty array when <audio> has no source', function(done) {
+		fixtureSetup('<audio autoplay="true" controls></audio>');
+
+		axe.utils
+			.preloadMedia({ treeRoot: axe._tree[0] })
+			.then(function(result) {
+				assert.equal(result.length, 0);
+				done();
+			})
+			.catch(function(err) {
+				done(err);
+			});
+	});
+
+	it.only('returns empty array when <video> has no source', function(done) {
 		fixtureSetup('<video id="target"><source src=""/></video>');
-		axe.utils.preloadMedia({ treeRoot: axe._tree[0] }).then(function(result) {
-			assert.equal(result.length, 0);
-			done();
-		});
+		axe.utils
+			.preloadMedia({ treeRoot: axe._tree[0] })
+			.then(function(result) {
+				assert.equal(result.length, 0);
+				done();
+			})
+			.catch(function(err) {
+				done(err);
+			});
 	});
 
 	it('returns media node (audio) after their metadata has been preloaded', function(done) {
@@ -63,29 +79,34 @@ describe.only('axe.utils.preloadMedia', function() {
 			'<audio src="/test/assets/moon-speech.mp3" autoplay="true" controls></audio>'
 		);
 
-		axe.utils.preloadMedia({ treeRoot: axe._tree[0] }).then(function(result) {
-			assert.equal(result.length, 1);
-			assert.isTrue(result[0].readyState > 0);
-			assert.equal(Math.round(result[0].duration), 27);
+		axe.utils
+			.preloadMedia({ treeRoot: axe._tree[0] })
+			.then(function(result) {
+				assert.equal(result.length, 1);
+				assert.isTrue(result[0].readyState > 0);
+				assert.equal(Math.round(result[0].duration), 27);
 
-			done();
-		});
+				done();
+			})
+			.catch(function(err) {
+				done(err);
+			});
 	});
 
-	(isIE11 ? it.skip : it)(
-		'returns media nodes (audio, video) after their metadata has been preloaded',
-		function(done) {
-			fixtureSetup(
-				// 1 audio elm
-				'<audio src="/test/assets/moon-speech.mp3"></audio>' +
-					// 1 video elm
-					'<video>' +
-					'<source src="/test/assets/video.mp4" type="video/mp4" />' +
-					'<source src="/test/assets/video.webm" type="video/webm" />' +
-					'</video>'
-			);
+	it.only('returns media nodes (audio, video) after their metadata has been preloaded', function(done) {
+		fixtureSetup(
+			// 1 audio elm
+			'<audio src="/test/assets/moon-speech.mp3"></audio>' +
+				// 1 video elm
+				'<video>' +
+				'<source src="/test/assets/video.mp4" type="video/mp4" />' +
+				'<source src="/test/assets/video.webm" type="video/webm" />' +
+				'</video>'
+		);
 
-			axe.utils.preloadMedia({ treeRoot: axe._tree[0] }).then(function(result) {
+		axe.utils
+			.preloadMedia({ treeRoot: axe._tree[0] })
+			.then(function(result) {
 				assert.equal(result.length, 2);
 				assert.isTrue(result[0].readyState > 0);
 				assert.equal(Math.round(result[0].duration), 27);
@@ -94,7 +115,9 @@ describe.only('axe.utils.preloadMedia', function() {
 				assert.equal(Math.round(result[1].duration), 14);
 
 				done();
+			})
+			.catch(function(err) {
+				done(err);
 			});
-		}
-	);
+	});
 });
